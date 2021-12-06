f73b31f3e075753615a645d198226c2f609c5c6b
Federico Sabato <federico.sabato1@hotmail.com>
2021-12-06 18:08:27 -0300

    Desafio 5

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-//Federico Sabato - Desafio 4
+//Federico Sabato - Desafio 5
 //Comienzo de Scripts
 
 const multiply= (x,y) => {return (x*y)}
+
+class crypto{
+    constructor(name,acr,price){
+        this.name=name;
+        this.acronym=acr;
+        this.price= price;
+    }
+}
+//Creo los objetos
+let btc = new crypto("Bitcoin","$BTC",54128.93);
+let eth = new crypto("Ethereum","$ETH",4140.45);
+let ada = new crypto("Cardano","$ADA",1.63);
+let sol = new crypto("Solana","$SOL",197.81);
+let shiba = new crypto("Shiba INU","$SHIBA",0.00004);
+//Obtengo los valores para el HTML
+document.getElementById("title1").innerHTML=btc.name; 
+document.getElementById("price1").innerHTML=btc.price;
+document.getElementById("title2").innerHTML=eth.name;
+document.getElementById("price2").innerHTML=eth.price;
+document.getElementById("title3").innerHTML=ada.name;
+document.getElementById("price3").innerHTML=ada.price;
+document.getElementById("title4").innerHTML=sol.name;
+document.getElementById("price4").innerHTML=sol.price;
+document.getElementById("title5").innerHTML=shiba.name;
+document.getElementById("price5").innerHTML=shiba.price;
 
 alert("¡Bienvendido a CRYPTO P2P MARKET!");
 
@@ -10,11 +35,10 @@ let pay;
 let quantity;
 
 switch((choice)){
-
     case "1":
         quantity = prompt("¿Cuantos $BTC desea comprar?\nPrecio: $54128.93");
         pay = multiply(54128.93,quantity);
-        alert("Usted pagará $"+pay );
+        alert("Usted pagará $"+pay);
         break;
     case "2":
         quantity = prompt("¿Cuantos $ETH desea comprar?\nPrecio: $4140.45");
